82e6a0b69b7ebc59c75c5436beaf0541d2afba07
Ben Do <bdo.appworkshop@gmail.com>
2025-12-06 16:46:25 -0600

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ben Do on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
