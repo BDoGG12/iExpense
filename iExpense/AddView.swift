dd4b987a3664c949ad0416fb96d9e75ae99eacfe
Ben Do <bdo.appworkshop@gmail.com>
2025-12-08 10:40:22 -0600

    Added AddView.

---
+++ iExpense/AddView.swift
@@ -0,0 +1,48 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Ben Do on 12/8/25.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @State private var name: String = ""
+    @State private var type: String = "Personal"
+    @State private var amount: Double = 0.0
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var expenses: Expenses
+    
+    let types = ["Personal", "Business"]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    var expenses = Expenses()
+    AddView(expenses: expenses)
+}
